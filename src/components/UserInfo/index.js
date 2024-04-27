dd74bb2bb453139982f05953d66d6d68b33e6c0a
kedar gaikwad <kedarga1@gmail.com>
2024-04-27 06:36:54 +0000

    my blog list successfully run

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png "
+      alt="profile"
+      className="user-profile-img"
+    />
+    <h1 className="user-heading">Wade Warren</h1>
+    <p className="user-details">Software Developer at UK</p>
+  </div>
+)
+export default UserInfo
